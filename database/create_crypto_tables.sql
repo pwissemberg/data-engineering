9ef96f951cd5065a16a4c9bb48fd7aaaf82202f9
pwissemberg <147991111+pwissemberg@users.noreply.github.com>
2025-05-07 00:57:55 +0200

    Create SQLite tables to store crypto hourly data

---
+++ database/create_crypto_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE crypto_hourly(
+
+    time DATETIME NOT NULL,
+    high FLOAT NOT NULL,
+    low FLOAT NOT NULL,
+    open FLOAT NOT NULL,
+    close FLOAT NOT NULL,
+    volume_from FLOAT NOT NULL,
+    volume_to FLOAT NOT NULL
+
+);
+
+INSERT INTO crypto_hourly (time, high, low, open, close, volume_from, volume_to) VALUES (-1, -1, -1, -1, -1, -1, -1);
+
+-- Test if table was created by inserting data
+SELECT * FROM crypto_hourly;
+
+-- Delete all records then
+-- N.B: To delete a table from a db, use DROP statement
+-- TRUNCATE TABLE crypto_hourly;
+DELETE FROM crypto_hourly;
+
+-- After Python data loading, check if data have been successfully loaded
+SELECT * FROM crypto_hourly;
