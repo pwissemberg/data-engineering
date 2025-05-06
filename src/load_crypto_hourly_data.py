5f0ba7e5ee5bbbcade01001d8e08cfa00b3c075e
pwissemberg <147991111+pwissemberg@users.noreply.github.com>
2025-05-07 00:59:26 +0200

    Upload crypto hourly data from csv file to database

---
+++ src/load_crypto_hourly_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import sqlite3
+
+
+hourly_df = pd.read_csv("../data/btc_hourly_data.csv")
+
+# I should have discarded the index before exporting BTC hourly data to csv
+hourly_df.drop(columns=["Unnamed: 0"], inplace=True)
+
+hourly_df = hourly_df[["time", "high", "low", "open", "close", "volumefrom", "volumeto"]]
+
+# Establish a connection with our database and
+# Create a "Cursor" which is an object to be able to interact with the database 
+# N.B: Note that the database has already been created
+connection = sqlite3.connect("../database/crypto_db.sqlite")
+cursor = connection.cursor()
+
+for _, row in hourly_df.iterrows():
+    
+    time = row["time"]
+    high = row["high"]
+    low = row["low"]
+    open = row["open"]
+    close = row["close"]
+    volume_from = row["volumefrom"]
+    volumeto = row["volumeto"]
+
+    cursor.execute("""
+                   INSERT INTO crypto_hourly (time, high, low, open, close, volume_from, volume_to) VALUES(?, ?, ?, ?, ?, ?, ?)
+                   """,
+                   (time, high, low, open, close, volume_from, volumeto)
+                )
+
+connection.commit()
+connection.close()
