051b79573a3cd29b97f41879c515f0998481cefb
pwissemberg <147991111+pwissemberg@users.noreply.github.com>
2025-05-10 17:21:11 +0200

    Implement new KPIs

--- database/postgresql/daily_kpis.sql
+++ database/postgresql/daily_kpis.sql
@@ -6,7 +6,7 @@ CREATE TABLE daily_kpis AS (
 
         SELECT
             
-            time,
+            date,
 
             CASE
                 WHEN volume_from != 0 THEN ROUND((volume_to / volume_from)::NUMERIC, 2)
@@ -35,9 +35,20 @@ CREATE TABLE daily_kpis AS (
             
             CASE
                 WHEN low != 0 THEN ROUND((100 * (high - low) / low)::NUMERIC, 2)
-            END AS low_price_spread_pct
+            END AS low_price_spread_pct,
 
-        FROM crypto_hourly_etl
+            -- Advanced KPIs
+            AVG(close) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS moving_avg,
+            STDDEV(close) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS volatility,
+            CASE
+                WHEN STDDEV(close) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) != 0 THEN (close - AVG(close) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)) / STDDEV(close) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+                ELSE NULL
+            END AS z_score,
+            MAX(high) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS rolling_max_high,
+            MIN(low) OVER (ORDER BY date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS rolling_min_low
+
+        FROM crypto_daily
+        GROUP BY date, high, low, open, close, volume_from, volume_to
 
     ),
 
@@ -46,7 +57,7 @@ CREATE TABLE daily_kpis AS (
 
         SELECT
 
-            time,
+            date,
 
             close > open AS is_green_candle,
 
@@ -58,9 +69,13 @@ CREATE TABLE daily_kpis AS (
             CASE
                 WHEN ABS((close - open)::NUMERIC) < high - low THEN TRUE
                 ELSE FALSE
-            END AS is_doji_like
+            END AS is_doji_like,
 
-        FROM crypto_hourly_etl
+            --Advanced KPIs
+            close > AVG(close) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS is_price_above_avg
+
+        FROM crypto_daily
+        GROUP BY date, high, low, open, close
 
     )
 
@@ -68,13 +83,19 @@ CREATE TABLE daily_kpis AS (
 
     SELECT     
         
-        p.time,
+        p.date,
         p.volume_weighted_avg_price,
         p.variation,
         p.variation_pct,
         p.price_spread,
         p.open_price_spread_pct,
         p.low_price_spread_pct,
+        p.moving_avg,
+        m.is_price_above_avg
+        p.volatility,
+        p.z_score,
+        p.rolling_max_high,
+        p.rolling_min_low
         m.is_green_candle,
         m.is_volatile,
         m.is_doji_like
@@ -82,9 +103,9 @@ CREATE TABLE daily_kpis AS (
     FROM price_kpis AS p
 
     LEFT JOIN market_kpis AS m
-        ON p.time = m.time
+        ON p.date = m.date
 
-    ORDER BY p.time ASC
+    ORDER BY p.date ASC
 
 
 
