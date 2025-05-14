a2b553e61c3ce5b7af03a7dc409f00b45d2bbb86
pwissemberg <147991111+pwissemberg@users.noreply.github.com>
2025-05-14 21:06:32 +0200

    Removing duplicates

--- database/dbt/models/1_staging/fct_crypto_hourly.sql
+++ database/dbt/models/1_staging/fct_crypto_hourly.sql
@@ -1,11 +1,49 @@
-SELECT
-    
-    TO_TIMESTAMP(time) AS time,
-    high,
-    low,
-    open,
-    close,
-    volumefrom AS volume_from,
-    volumeto AS volume_to
+WITH hourly_raw AS (
 
-FROM {{ source('crypto_api', 'crypto_hourly_elt') }}
+    SELECT
+        
+        time,
+        high,
+        low,
+        open,
+        close,
+        volumefrom AS volume_from,
+        volumeto AS volume_to
+
+    FROM {{ source('crypto_api', 'crypto_hourly_elt') }}
+
+),
+
+
+
+hourly_processed AS (
+
+    SELECT
+        
+        TO_TIMESTAMP(time) AS time,
+        high,
+        low,
+        open,
+        close,
+        volume_from,
+        volume_to
+
+    FROM hourly_raw
+
+),
+
+
+
+hourly_final AS (
+
+    SELECT
+
+        DISTINCT *
+
+    FROM hourly_processed
+
+)
+
+
+
+SELECT * FROM hourly_final
